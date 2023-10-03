5d75b939bc0a6579484b1f19342c5e3f271aefcc
vikas99341 <48900898+vikas99341@users.noreply.github.com>
2023-10-03 18:48:07 +0530

    Updated cidr range on variables.tf

--- variables.tf
+++ variables.tf
@@ -2,10 +2,10 @@ variable "region" {
   default = "us-east-1"
 }
 variable "vpc_dev_cidr" {
-  default = "172.17.0.0/16"
+  default = "172.19.0.0/16"
 }
 variable "vpc_prod_cidr" {
-  default = "172.16.0.0/16"
+  default = "172.18.0.0/16"
 }
 variable "tenancy" {
   default = "default"
